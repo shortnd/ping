776c58a6bc2b6aae9a360698285e178af3a9ffd0
Collin OConnell <ococncol@gmail.com>
2019-05-31 15:01:01 -0400

    updated scripts

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 
 url = 'http://www.google.com'
 
-soup = BeautifulSoup(urlopen('google.com').read(), features="lxml")
+soup = BeautifulSoup(urlopen('http://cms2.revize.com/').read(), features="lxml")
 print(soup.title.string)
