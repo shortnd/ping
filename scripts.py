ad49f7d8aef8ecc8f19a160ecba63e128ae9a806
Collin OConnell <ococncol@gmail.com>
2019-11-04 15:54:51 -0500

    updated because of error

--- scripts.py
+++ scripts.py
@@ -27,36 +27,68 @@ with open('urls.txt', 'r') as f:
 
         try:
             response = socket.gethostbyname(no_backslash)
-
             if '69.64.88.239' in response:
-                print(f'DNS not Changed - {no_backslash}')
-                not_changed_file.write(f'DNS not Changed - {no_backslash}\n')
+                print(f'69.64.88.239 {no_backslash}')
             elif '50.28.0.27' in response:
                 try:
-                    soup = BeautifulSoup(
-                        urlopen('http://'+no_http).read(), features="lxml")
+                    soup = BeautifulSoup(urlopen(f'http://{no_http}').read(), features="html.parser")
                 except:
-                    soup = BeautifulSoup(
-                        urlopen('https://'+no_http).read(), features="lxml")
+                    soup = BeautifulSoup(urlopen(f'https://{no_http}').read(), features="html.parser")
                 if soup.title.string:
                     if 'IIS' in soup.title.string:
                         print(f'{no_backslash} is Down')
-                        changed_file.write(
-                            f'On new server but down - {no_backslash}')
                     else:
-                        pass
-                print(f'DNS changed - {no_backslash}')
-                changed_file.write(f'DNS changed - {no_backslash}\n')
+                        print(f'DNS Changed - {no_backslash}')
             else:
-                print(f'on other server - {no_backslash}')
-                error_or_other_server.write(
-                    f'On different server - {no_backslash}\n')
-
+                print(f'On other server {no_backslash}')
         except:
-            try:
-                soup = BeautifulSoup(
-                    urlopen('http://'+no_backslash+'/').read(), features="lxml")
-                print(f'Error on {no_backslash}')
-            except:
-                print(f'Error on {no_backslash}')
-            error_or_other_server.write(f'Error on {no_backslash}\n')
+            print(f'Ops')
+    # for line in f:
+    #     hostname = line.strip()
+    #     if "http" in hostname:
+    #         no_http = hostname.replace("http://", "")
+    #     elif "https" in hostname:
+    #         no_http = hostname.replace("https://", "")
+    #     else:
+    #         no_http = hostname
+
+    #     if no_http.endswith('/'):
+    #         no_backslash = no_http.rsplit('/', 1)[0]
+    #     else:
+    #         no_backslash = no_http
+
+    #     try:
+    #         response = socket.gethostbyname(no_backslash)
+
+    #         if '69.64.88.239' in response:
+    #             print(f'DNS not Changed - {no_backslash}')
+    #             not_changed_file.write(f'DNS not Changed - {no_backslash}\n')
+    #         elif '50.28.0.27' in response:
+    #             try:
+    #                 soup = BeautifulSoup(
+    #                     urlopen('http://'+no_http).read(), features="lxml")
+    #             except:
+    #                 soup = BeautifulSoup(
+    #                     urlopen('https://'+no_http).read(), features="lxml")
+    #             if soup.title.string:
+    #                 if 'IIS' in soup.title.string:
+    #                     print(f'{no_backslash} is Down')
+    #                     changed_file.write(
+    #                         f'On new server but down - {no_backslash}')
+    #                 else:
+    #                     pass
+    #             print(f'DNS changed - {no_backslash}')
+    #             changed_file.write(f'DNS changed - {no_backslash}\n')
+    #         else:
+    #             print(f'on other server - {no_backslash}')
+    #             error_or_other_server.write(
+    #                 f'On different server - {no_backslash}\n')
+
+    #     except:
+    #         try:
+    #             soup = BeautifulSoup(
+    #                 urlopen('http://'+no_backslash+'/').read(), features="lxml")
+    #             print(f'Error on {no_backslash}')
+    #         except:
+    #             print(f'Error on {no_backslash}')
+    #         error_or_other_server.write(f'Error on {no_backslash}\n')
